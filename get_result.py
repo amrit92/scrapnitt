572331d8200695f1e05b9c982e71429f61858c98
B-Prashanth <prashanthreddybilla@gmail.com>
2013-12-28 10:32:41 +0530

    Updating the two python files (get_result.py and run.py) to write the output to excel file and a separte doc file

--- get_result.py
+++ get_result.py
@@ -1,8 +1,11 @@
 import mechanize
+import xlutils
 import cookielib
 import sys
 import subprocess
 import xlwt
+import xlrd
+from xlutils.copy import copy
 # Browser
 br = mechanize.Browser()
 
@@ -23,38 +26,42 @@ response = br.submit()
 #(contain.split(">")[1]).split("<")[0];
 br.select_form("Form1")
 br.set_all_readonly(False)
-
 br["__EVENTTARGET"] = 'Dt1'
 br["Dt1"]=["88"]
 br.find_control("Button1").disabled = True
 output = br.submit().read()
-
+count = int(sys.argv[1]) - int("106110000")
 regex_gpa = "id=\"LblGPA\""
 regex_rollnum = "id=\"LblEnrollmentNo\""
 regex_name = "id=\"LblName\""
 writefile = open("outputfile","w")
-fileio = open("OUTPUT.xls","a")
+fileio = open("OUTPUT.doc","a")
 writefile.write(output)
 writefile.close()
 textfile = open("outputfile","r")
-book = xlwt.Workbook()
-sheet1 = book.add_sheet("sheetmy")
-sheet1.write(0, 0, "Name")
-sheet1.write(0, 1, "Roll number")
-sheet1.write(0, 2, "Gpa")
-book.save("trial.xls")
+workbook = xlrd.open_workbook('result.xls')
+mysheet_read = workbook.sheet_by_index(0)
+mysheet_write = copy(workbook)
+mysheet = mysheet_write.get_sheet(0)
+mysheet.write(0, 0, "Name")
+mysheet.write(0, 1, "Roll number")
+mysheet.write(0, 2, "Gpa")
 for line in textfile.readlines():
 	if regex_rollnum in line:
 		text1 = "  Roll number  : " + ((line.split(">")[3]).split("<"))[0]
 		fileio.write(text1)
 		fileio.write("\n")
+		mysheet.write(count, 1, ((line.split(">")[3]).split("<"))[0])
 	elif regex_name in line:
 		text2 = "  Name  : " + ((line.split(">")[3]).split("<"))[0]
 		fileio.write(text2)
 		fileio.write("\n")
+		mysheet.write(count, 0, ((line.split(">")[3]).split("<"))[0])
 	elif regex_gpa in line:
 		text3 = "  GPA  : " + ((line.split(">")[3]).split("<"))[0]
 		fileio.write(text3)
 		fileio.write("\n")
 		fileio.write("\n")
+		mysheet.write(count, 2, ((line.split(">")[3]).split("<"))[0])
+mysheet_write.save("result.xls")
 
--- run.py
+++ run.py
@@ -1,5 +1,11 @@
 import subprocess
+import xlwt
 value = 106110000
+book = xlwt.Workbook()
+sheet1 = book.add_sheet("excelsheet")
+col1 = sheet1.col(0)
+col1.width = 256*40
+book.save("result.xls")
 for j in xrange(1,107):
 	newvalue = str(value + j)
 	subprocess.call(" python get_result.py "+newvalue, shell=True)
